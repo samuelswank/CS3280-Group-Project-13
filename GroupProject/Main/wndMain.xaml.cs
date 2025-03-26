4311b39b980650a2be5cc49fcc28d8a0ed1eca90
samuelswank <samswank@gmail.com>
2025-03-26 14:30:42 -0600

    Initial commit

---
+++ GroupProject/Main/wndMain.xaml.cs
@@ -0,0 +1,34 @@
+﻿using GroupProject.Search;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupProject.Main
+{
+    /// <summary>
+    /// Interaction logic for wndMain.xaml
+    /// </summary>
+    public partial class wndMain : Window
+    {
+        public wndMain()
+        {
+            InitializeComponent();
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            wndSearch wndSearch = new wndSearch();
+            wndSearch.ShowDialog();
+            this.Show();
+        }
+    }
+}
