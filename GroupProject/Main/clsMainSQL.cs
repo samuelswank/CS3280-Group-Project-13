56252ea7104776a77babe4887d5a9f4256a3b3e9
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-03-29 14:58:29 -0600

    Created All the SQL Methods, Created the UI for the Main Window, Added the comments per assignment requirements.

--- GroupProject/Main/clsMainSQL.cs
+++ GroupProject/Main/clsMainSQL.cs
@@ -3,10 +3,227 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Reflection;
 
 namespace GroupProject.Main
 {
-    internal class clsMainSQL
+    public class clsMainSQL
     {
+        private string sConnectionString;
+
+        public clsMainSQL()
+        {
+            sConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data source= " + Directory.GetCurrentDirectory() + "\\Invoice.mdb";
+        }
+        /// <summary>
+        /// Updates the total cost of a specific invoice.
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <param name="newTotal"></param>
+        /// <returns></returns>
+        public int UpdateTotal(int invoiceNum, double newTotal)
+        {
+            string sSQL = $"UPDATE Invoices SET TotalCost = {newTotal} WHERE InvoiceNum = {invoiceNum}";
+            return ExecuteNonQuery(sSQL);
+        }
+        /// <summary>
+        /// Inserts a new line item into an invoice.
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <param name="lineItemNum"></param>
+        /// <param name="itemCode"></param>
+        /// <returns></returns>
+        public int InsertLineItems(int invoiceNum, int lineItemNum, string itemCode)
+        {
+            string sSQL = $"INSERT INTO LineItems (InvoiceNum, LineItemNum, ItemCode) VALUES ({invoiceNum}, {lineItemNum}, '{itemCode})'";
+            return ExecuteNonQuery(sSQL);
+        }
+        /// <summary>
+        /// Inserts a new invoice.
+        /// </summary>
+        /// <param name="invoiceDate"></param>
+        /// <param name="totalCost"></param>
+        /// <returns></returns>
+        public int InsertInvoice(string invoiceDate, int totalCost)
+        {
+            string sSQL = $"INSERT INTO Invoices (InvoiceDate, TotalCost) VALUES (#{invoiceDate}#, {totalCost})'";
+            return ExecuteNonQuery(sSQL);
+        }
+        /// <summary>
+        /// Gets invoice details by InvoiceNum.
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <returns></returns>
+        public DataSet GetInvoice(int invoiceNum)
+        {
+            int iRetVal = 0;
+            string sSQL = $"SELECT InvoiceNum, InvoiceDate, TotalCost FROM Invoices WHERE InvoiceNum = {invoiceNum}";
+            return ExecuteSQLStatement(sSQL, ref iRetVal);
+        }
+        /// <summary>
+        /// Gets all available items from ItemDesc table.
+        /// </summary>
+        /// <returns></returns>
+        public DataSet GetAllItem()
+        {
+            int iRetVal = 0;
+            string sSQL = $"SELECT ItemCode, ItemDesc, Cost FROM ItemDesc";
+            return ExecuteSQLStatement(sSQL, ref iRetVal);
+        }
+        /// <summary>
+        /// Gets all line items for a specific invoice.
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <returns></returns>
+        public DataSet GetInvoiceLineItems(int invoiceNum)
+        {
+            int iRetVal = 0;
+            string sSQL = $"SELECT LineItems.ItemCode, ItemDesc.ItemDesc, ItemDesc.Cost " +
+                          $"FROM LineItems, ItemDesc " +
+                          $"WHERE LineItems.ItemCode = ItemDesc.ItemCode " +
+                          $"AND LineItems.InvoiceNum = {invoiceNum}";
+            return ExecuteSQLStatement(sSQL, ref iRetVal);
+        }
+        /// <summary>
+        /// Deletes all line items from a specific invoice.
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <returns></returns>
+        public int DeleteInvoiceLineItems(int invoiceNum)
+        {
+            string sSQL = $"DELETE FROM LineItems WHERE InvoiceNum = {invoiceNum}";
+            return ExecuteNonQuery(sSQL);
+        }
+
+
+
+        /// <summary>
+        /// This method takes an SQL statment that is passed in and executes it.  The resulting values
+        /// are returned in a DataSet.  The number of rows returned from the query will be put into
+        /// the reference parameter iRetVal.
+        /// </summary>
+        /// <param name="sSQL">The SQL statement to be executed.</param>
+        /// <param name="iRetVal">Reference parameter that returns the number of selected rows.</param>
+        /// <returns>Returns a DataSet that contains the data from the SQL statement.</returns>
+        private DataSet ExecuteSQLStatement(string sSQL, ref int iRetVal)
+        {
+            try
+            {
+                //Create a new DataSet
+                DataSet ds = new DataSet();
+
+                using (OleDbConnection conn = new OleDbConnection(sConnectionString))
+                {
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter())
+                    {
+
+                        //Open the connection to the database
+                        conn.Open();
+
+                        //Add the information for the SelectCommand using the SQL statement and the connection object
+                        adapter.SelectCommand = new OleDbCommand(sSQL, conn);
+                        adapter.SelectCommand.CommandTimeout = 0;
+
+                        //Fill up the DataSet with data
+                        adapter.Fill(ds);
+                    }
+                }
+
+                //Set the number of values returned
+                iRetVal = ds.Tables[0].Rows.Count;
+
+                //return the DataSet
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method takes an SQL statment that is passed in and executes it.  The resulting single 
+        /// value is returned.
+        /// </summary>
+        /// <param name="sSQL">The SQL statement to be executed.</param>
+        /// <returns>Returns a string from the scalar SQL statement.</returns>
+        private string ExecuteScalarSQL(string sSQL)
+        {
+            try
+            {
+                //Holds the return value
+                object obj;
+
+                using (OleDbConnection conn = new OleDbConnection(sConnectionString))
+                {
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter())
+                    {
+
+                        //Open the connection to the database
+                        conn.Open();
+
+                        //Add the information for the SelectCommand using the SQL statement and the connection object
+                        adapter.SelectCommand = new OleDbCommand(sSQL, conn);
+                        adapter.SelectCommand.CommandTimeout = 0;
+
+                        //Execute the scalar SQL statement
+                        obj = adapter.SelectCommand.ExecuteScalar();
+                    }
+                }
+
+                //See if the object is null
+                if (obj == null)
+                {
+                    //Return a blank
+                    return "";
+                }
+                else
+                {
+                    //Return the value
+                    return obj.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method takes an SQL statment that is a non query and executes it.
+        /// </summary>
+        /// <param name="sSQL">The SQL statement to be executed.</param>
+        /// <returns>Returns the number of rows affected by the SQL statement.</returns>
+        private int ExecuteNonQuery(string sSQL)
+        {
+            try
+            {
+                //Number of rows affected
+                int iNumRows;
+
+                using (OleDbConnection conn = new OleDbConnection(sConnectionString))
+                {
+                    //Open the connection to the database
+                    conn.Open();
+
+                    //Add the information for the SelectCommand using the SQL statement and the connection object
+                    OleDbCommand cmd = new OleDbCommand(sSQL, conn);
+                    cmd.CommandTimeout = 0;
+
+                    //Execute the non query SQL statement
+                    iNumRows = cmd.ExecuteNonQuery();
+                }
+
+                //return the number of rows affected
+                return iNumRows;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
     }
 }
--- GroupProject/Main/wndMain.xaml.cs
+++ GroupProject/Main/wndMain.xaml.cs
@@ -17,18 +17,47 @@ namespace GroupProject.Main
     /// </summary>
     public partial class wndMain : Window
     {
+        /// <summary>
+        /// The constructor is initialized and also will completely shutdown if user clicks X on top right
+        /// </summary>
         public wndMain()
         {
             InitializeComponent();
             Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
         }
 
-        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// When user clicks exit inside file menu control, the program closes entirely
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        /// <summary>
+        /// When user clicks this button, the search window opens up
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchInvoice_Click(object sender, RoutedEventArgs e)
         {
             this.Hide();
             wndSearch wndSearch = new wndSearch();
             wndSearch.ShowDialog();
             this.Show();
+            //The main window will grab the invoice number from the search window property, and then load it into the datagrid and have it disabled for any edits.
+
         }
-    }
+        /// <summary>
+        /// When user clicks this button, The items window opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditItems_Click(object sender, RoutedEventArgs e)
+        {
+            //Once edit items window is hidden, if the value of HasItemBeenChanged is true, then update the items combo box
+        }
+    
+}
 }
