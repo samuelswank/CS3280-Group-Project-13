0300a7a09ce5df74334c183ea799e785a768107a
samuelswank <samswank@gmail.com>
2025-04-18 13:53:27 -0600

    namespace GroupProject

--- GroupProject/Common/ExceptionHandler.cs
+++ GroupProject/Common/ExceptionHandler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment6
+namespace GroupProject
 {
     /// <summary>
     /// Class which handles exceptions for top-level methods
